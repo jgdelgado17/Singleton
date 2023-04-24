e1411b60cb88c8f026ced2be42b3115bf1c789b7
jgdelgado17 <jegadele@hotmail.com>
2023-04-24 10:32:02 -0500

    Second commit

--- src/App.java
+++ src/App.java
@@ -1,5 +1,17 @@
+import implementaciones.SingletonThreadSafeCheckedLocking;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        // System.out.println("Hello, World!");
+        SingletonThreadSafeCheckedLocking singleton1 = SingletonThreadSafeCheckedLocking.getInstance();
+        SingletonThreadSafeCheckedLocking singleton2 = SingletonThreadSafeCheckedLocking.getInstance();
+
+        System.out.println(singleton1);
+        System.out.println(singleton2);
+
+        if (singleton1 == singleton2)
+            System.out.println("Son el mismo objeto");
+        else
+            System.out.println("No son el mismo objeto");
     }
 }
